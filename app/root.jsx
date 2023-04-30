bce3386628dd6a3d17bde9117dac8189973f057b
Brian Cameron <bcameron1489@gmail.com>
2023-04-30 12:11:49 -0400

    Add button and functionality for cart drawer

--- app/root.jsx
+++ app/root.jsx
@@ -10,6 +10,27 @@ import tailwind from './styles/tailwind-build.css';
 import styles from './styles/app.css';
 import favicon from '../public/favicon.svg';
 import {Layout} from './components/Layout';
+import {defer} from '@shopify/remix-oxygen';
+import {CART_QUERY} from '~/queries/cart';
+
+
+async function getCart({storefront}, cartId) {
+  if (!storefront) {
+    throw new Error('missing storefront client in cart query');
+  }
+
+  const {cart} = await storefront.query(CART_QUERY, {
+    variables: {
+      cartId,
+      country: storefront.i18n.country,
+      language: storefront.i18n.language,
+    },
+    cache: storefront.CacheNone(),
+  });
+
+  return cart;
+}
+
 
 export const links = () => {
   return [
@@ -27,10 +48,15 @@ export const links = () => {
   ];
 };
 
-export async function loader({context}) {
-  const layout = await context.storefront.query(LAYOUT_QUERY);
-  return {layout};
+export async function loader({context, request}) {
+  const cartId = await context.session.get('cartId');
+
+  return defer({
+    cart: cartId ? getCart(context, cartId) : undefined,
+    layout: await context.storefront.query(LAYOUT_QUERY),
+  });
 }
+
 
 export default function App() {
   const data = useLoaderData();
