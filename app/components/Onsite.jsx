f6645e8961f13b569125501f51891759b79800a7
Brian Cameron <bcameron1489@gmail.com>
2023-05-03 13:50:46 -0400

    update component

---
+++ app/components/Onsite.jsx
@@ -0,0 +1,21 @@
+
+
+export default function trackViewedProduct(klproduct) {
+
+        var _learnq = window._learnq || [];
+        var prod = {
+            Name: klproduct.title,
+            ProductID: klproduct.id.substring(klproduct.id.lastIndexOf('/') + 1),
+            Categories:
+                klproduct.collections == undefined
+                ? null
+                : klproduct.collections.edges.map((a) => a.node.title),
+            ImageURL: klproduct.media.nodes[0].image.url,
+            URL: klproduct.url,
+            Brand: klproduct.vendor,
+            Price: klproduct.variants.nodes[0].price.amount
+    };
+    _learnq.push(['track', 'Viewed Product', prod]);
+
+    console.log('component is rendering')
+  }
